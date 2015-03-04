21bba41dd0691cbd453133ee8c93e3f36d37f922
tqvarnst <tqvarnst@redhat.com>
2015-03-04 17:20:39 +0100

    Fixes #3

--- src/main/java/org/jbossdemocentral/service/StartupBean.java
+++ src/main/java/org/jbossdemocentral/service/StartupBean.java
@@ -1,10 +1,14 @@
 package org.jbossdemocentral.service;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.ejb.TimerService;
 import javax.inject.Inject;
 
 import org.infinispan.Cache;
@@ -15,14 +19,24 @@ import org.jbossdemocentral.model.DemoDao;
 @Startup
 public class StartupBean {
 
+	Logger log = Logger.getLogger(this.getClass().getName());
+	
 	@Inject
 	DemoDao demoDao;
+	
+	@Resource
+	private TimerService timerService;
 	
 	@Inject
 	Cache<String,List<Demo>> cache;
 	
 	@PostConstruct
 	void init() {
+		cache.put(DemoRestService.DEMO_LIST_CACHE_ENTRY_KEY, demoDao.getDemos());
+	}
+	
+	@Schedule(hour="*")
+	void refreshCache() {
 		cache.put(DemoRestService.DEMO_LIST_CACHE_ENTRY_KEY, demoDao.getDemos());
 	}
 
